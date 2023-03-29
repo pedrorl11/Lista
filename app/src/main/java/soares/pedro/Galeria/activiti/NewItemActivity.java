a03d7f06f2fb7a4c76db50b5d14190eba57e5099
20211tiimi0125 <pedro05042006@gmail.com>
2023-03-29 12:58:58 -0300

    aula 29

---
+++ app/src/main/java/soares/pedro/Galeria/activiti/NewItemActivity.java
@@ -0,0 +1,33 @@
+package soares.pedro.Galeria.activiti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import soares.pedro.lista.R;
+
+public class NewItemActivity extends AppCompatActivity {
+
+    static int PHOTO_PICKER_REQUEST = 1;
+    Uri photoSelected = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_item);
+
+        ImageButton imgCI = findViewById(R.id.imBCI);
+        imgCI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST);
+            }
+        });
+    }
+}
